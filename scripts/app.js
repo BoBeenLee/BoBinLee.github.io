21b1462de0cb111e8422e62cc704bbb6b73c5953
BoBinLee <ad1053272>
2014-08-15 23:14:37 +0900

    깃 허브 초기 화면 세팅 - 2

--- scripts/app.js
+++ scripts/app.js
@@ -9,25 +9,39 @@
  * Main module of the application.
  */
 angular
-  .module('meanstackApp', [
-    'ngAnimate',
-    'ngCookies',
-    'ngResource',
-    'ngRoute',
-    'ngSanitize',
-    'ngTouch'
-  ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+    .module('meanstackApp', [
+        'ngAnimate',
+        'ngCookies',
+        'ngResource',
+        'ngRoute',
+        'ngSanitize',
+        'ngTouch'
+    ])
+    .config(function ($routeProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: 'views/main.html',
+                controller: 'MainCtrl'
+            })
+            .when('/about', {
+                templateUrl: 'views/about.html',
+                controller: 'AboutCtrl'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+    }).controller('HomeCtrl', function ($scope) {
+        $scope.init = function () {
+            // rumble
+            $('.rumble').jrumble({
+                x: 15,
+                y: 5,
+                rotation: 4,
+                speed: 120
+            });
+            $('.rumble').trigger('startRumble');
+            //  alert(jQuery(".category-content").html());
+        }
+    });
+
+
---
+++ scripts/controllers/contents/competition.js
@@ -0,0 +1,37 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name meanstackApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the meanstackApp
+ */
+angular.module('meanstackApp')
+    .controller('CompetitionCtrl', function ($scope) {
+        $scope.images = [
+            '1.png',
+            '2.png',
+            '3.png',
+            '4.png'
+        ];
+
+        $scope.init = function () { }
+
+        $scope.refresh = function (last) {
+            if (last) {
+                $('.flexslider').flexslider({
+                    animation: "slide",
+                    animationLoop: false
+                });
+                // zoom
+                $('.zoom').zoom({
+                    magnify : 1,
+                    on: 'click' });
+            }
+        }
+    }).directive('repeatDone', function () {
+        return function (scope, element, attrs) {
+        };
+    });
+
