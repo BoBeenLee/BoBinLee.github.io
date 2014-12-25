9dcb664cc64e5f03eaca832e3e53d337562b0d73
BoBinLee <ad1053272>
2014-12-25 20:45:51 +0900

    내용 추가

---
+++ scripts/contents/skhucalc.js
@@ -0,0 +1,61 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name meanstackApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the meanstackApp
+ */
+angular.module('meanstackApp')
+  .controller('SkhucalcCtrl', function ($scope) {
+    $scope.images = [
+      'main.png',
+      'type1.png',
+      'type2.png',
+      'input1.png',
+      'input2.png',
+      'result1.png',
+      'result2.png',
+      'mail.png'
+    ];
+
+    $scope.init = function () {
+      $scope.isOne = false;
+    };
+
+    $scope.refresh = function () {
+      if (!$scope.isOne) {
+        $scope.isOne = true;
+
+        $('.slides').bxSlider({
+          auto: true,
+          adaptiveHeight: true,
+          mode: 'fade'
+        });
+        $('.box-img').colorbox({
+          rel: 'box-img',
+          transition: "fade",
+          innerWidth: "90%",
+          innerHeight: "90%",
+          current: "",
+          next: "",
+          previous: "",
+          onLoad: function () {
+//                        var idx = $(this).attr("content").trim();
+////                        alert(val("12"));
+//                        $('.flexslider').flexslider(Number(idx));
+          },
+          onComplete: function () {
+            $('#cboxLoadedContent img').on("click", function () {
+              $.colorbox.close();
+            });
+          }
+        });
+      }
+    }
+  }).directive('repeatDone', function () {
+    return function (scope, element, attrs) {
+
+    };
+  });
--- scripts/main.js
+++ scripts/main.js
@@ -8,70 +8,74 @@
  * Controller of the meanstackApp
  */
 angular.module('meanstackApp')
-    .controller('MainCtrl', function ($scope, $window) {
-        $scope.contents = [
-            {
-                name: '학과 홈페이지',
-                url: '/views/contents/softhome.html'
-            },
-            {
-                name: '경진대회시스템',
-                url: '/views/contents/competition.html'
-            },
-            {
-                name: '하우스',
-                url: '/views/contents/house.html'
-            },
-            {
-                name: '성공회알람앱',
-                url: '/views/contents/skhualarm.html'
-            }
-        ];
+  .controller('MainCtrl', function ($scope, $window) {
+    $scope.contents = [
+      {
+        name: '학과 홈페이지',
+        url: '/views/contents/softhome.html'
+      },
+      {
+        name: '경진대회시스템',
+        url: '/views/contents/competition.html'
+      },
+      {
+        name: '하우스',
+        url: '/views/contents/house.html'
+      },
+      {
+        name: '성공회알람앱',
+        url: '/views/contents/skhualarm.html'
+      },
+      {
+        name: '입학상담앱',
+        url: '/views/contents/skhucalc.html'
+      }
+    ];
 
-        $scope.bottomMenus = [
-            {
-                name : "home",
-                img : "/images/main/home_icon.svg"
-            } ,
-            {
-                name : "about me",
-                img : "/images/main/me_icon.svg"
-            } ,
-            {
-                name : "portfolio",
-                img : "/images/main/port_icon.svg"
-            } ,
-            {
-                name : "contact",
-                img : "/images/main/contact_icon.svg"
-            }
-        ];
+    $scope.bottomMenus = [
+      {
+        name: "home",
+        img: "/images/main/home_icon.svg"
+      },
+      {
+        name: "about me",
+        img: "/images/main/me_icon.svg"
+      },
+      {
+        name: "portfolio",
+        img: "/images/main/port_icon.svg"
+      },
+      {
+        name: "contact",
+        img: "/images/main/contact_icon.svg"
+      }
+    ];
 
-        $scope.content = null;
-        $scope.selectedIndex = 0;
+    $scope.content = null;
+    $scope.selectedIndex = 0;
 
-        $scope.init = function() {
-            // sausage
-            $(window).sausage({
-                content: function (i, $page) {
-                    // $page.find('.side-tag').first().text()
-                    var sausageSpan = '<span class="sausage-span"><img width="28" height="28" src="' + $scope.bottomMenus[i].img  + '" /></span>'
-                    return sausageSpan;
-                }
-            });
+    $scope.init = function () {
+      // sausage
+      $(window).sausage({
+        content: function (i, $page) {
+          // $page.find('.side-tag').first().text()
+          var sausageSpan = '<span class="sausage-span"><img width="28" height="28" src="' + $scope.bottomMenus[i].img + '" /></span>'
+          return sausageSpan;
+        }
+      });
 
-            // rumble
-            $('.rumble').jrumble({
-                x: 15,
-                y: 5,
-                rotation: 4,
-                speed: 120
-            });
-            $('.rumble').trigger('startRumble');
+      // rumble
+      $('.rumble').jrumble({
+        x: 15,
+        y: 5,
+        rotation: 4,
+        speed: 120
+      });
+      $('.rumble').trigger('startRumble');
 
-            // settings
-            $scope.setContent(0);
-        };
+      // settings
+      $scope.setContent(0);
+    };
 
 //        $scope.resizeAdjustments = function() {
 //            if ($window.innerWidth < 768 && !$scope.isMobile) {
@@ -84,12 +88,12 @@ angular.module('meanstackApp')
 //        $window.onresize = function () {
 //            $scope.resizeAdjustments();
 //        }
-        $scope.onBottomMenuClick = function(index){
-            angular.element(".sausage-set .sausage:nth-of-type(" + index + ")").trigger("click");
-        }
+    $scope.onBottomMenuClick = function (index) {
+      angular.element(".sausage-set .sausage:nth-of-type(" + index + ")").trigger("click");
+    }
 
-        $scope.setContent = function (index) {
-            $scope.selectedIndex = index;
-            $scope.content = $scope.contents[index];
-        };
-    });
+    $scope.setContent = function (index) {
+      $scope.selectedIndex = index;
+      $scope.content = $scope.contents[index];
+    };
+  });
