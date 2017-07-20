80be220c04e6169ca7debf9aa18dcca7d9e9cbdd
bobinlee <cultist_tp@naver.com>
2017-07-20 23:46:44 +0900

    블로그 RSS 추가.

--- components/Blog/Blog.js
+++ components/Blog/Blog.js
@@ -1,31 +1,74 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
-import Tistory from 'tistory-api';
+import {
+  Card, CardHeader, CardText, List, ListItem,
+  RaisedButton,
+} from 'material-ui';
+import _ from 'lodash';
+import moment from 'moment';
+import './Blog.scss';
 
-const propTypes = {};
+const propTypes = {
+  url: PropTypes.string,
+};
 
-const defaultProps = {};
+const defaultProps = {
+  url: 'http://cultist-tp.tistory.com/rss',
+};
 
 class Blog extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      articles: []
+    };
   }
 
   componentDidMount() {
-
-
+    this.requestTistoryRSS();
   }
 
+  requestTistoryRSS = () => {
+    const { url } = this.props;
+    feednami.load(url, (res) => {
+      const articles = _.map(res.feed.entries, article => {
+        return {
+          guid : article.guid,
+          title : article.title,
+          link : article.link,
+          date : article.date,
+          description: article.description,
+        };
+      });
+      this.setState({articles});
+    });
+  };
+
   render() {
-    const params = {
-      access_token: 'abc...XYZ', // put tistory's access token.
-      output: 'json'
-    };
-
-    const tistory = new Tistory(params);
-
+    const { url } = this.props;
+    const articles = this.state.articles.map(article => {
+      const title = (<a href={article.link} className="blog-title">{article.title}</a>);
+      const date = (<div className="blog-date">{moment(article.date).format('YYYY-MM-DD HH:mm:ss')}</div>);
+      const description = (<div dangerouslySetInnerHTML={{__html: article.description}}></div>);
+      return (
+        <Card key={article.guid} className="blog-card">
+          <CardHeader
+            title={title}
+            subtitle={date}
+            showExpandableButton={true}
+          />
+          <CardText className="blog-card-content" expandable={true}>
+            {description}
+          </CardText>
+        </Card>)
+    });
     return (
-      <div>Blog</div>
+      <div className="blog">
+        <List>
+          {articles}
+          <RaisedButton href={url} label="More" primary className="blog-more-button" />
+        </List>
+      </div>
     );
   }
 }
