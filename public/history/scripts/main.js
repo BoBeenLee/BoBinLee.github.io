3bbfe3e9dbb84e1c4711adb2f08778d6ed5e4fb6
bobinlee <cultist_tp@naver.com>
2017-07-25 15:34:44 +0900

    이전 포 살리기.

--- public/history/scripts/main.js
+++ public/history/scripts/main.js
@@ -12,46 +12,46 @@ angular.module('meanstackApp')
     $scope.contents = [
       {
         name: '학과 홈페이지',
-        url: '/views/contents/softhome.html'
+        url: 'views/contents/softhome.html'
       },
       {
         name: '경진대회시스템',
-        url: '/views/contents/competition.html'
+        url: 'views/contents/competition.html'
       },
       {
         name: '하우스',
-        url: '/views/contents/house.html'
+        url: 'views/contents/house.html'
       },
       {
         name: '성공회알람앱',
-        url: '/views/contents/skhualarm.html'
+        url: 'views/contents/skhualarm.html'
       },
       {
         name: '입학상담앱',
-        url: '/views/contents/skhucalc.html'
+        url: 'views/contents/skhucalc.html'
       },
       {
         name: '중간지점앱',
-        url: '/views/contents/intersection.html'
+        url: 'views/contents/intersection.html'
       }
     ];
 
     $scope.bottomMenus = [
       {
         name: "home",
-        img: "/images/main/home_icon.svg"
+        img: "images/main/home_icon.svg"
       },
       {
         name: "about me",
-        img: "/images/main/me_icon.svg"
+        img: "images/main/me_icon.svg"
       },
       {
         name: "portfolio",
-        img: "/images/main/port_icon.svg"
+        img: "images/main/port_icon.svg"
       },
       {
         name: "contact",
-        img: "/images/main/contact_icon.svg"
+        img: "images/main/contact_icon.svg"
       }
     ];
 
